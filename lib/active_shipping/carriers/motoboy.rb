12eb4d832a78aafbdfb7de984a0a7329e2e3bd08
Sanderson Santana <sanderson@digaeita.com.br>
2016-07-11 10:36:27 -0300

    adicionado método de entrega por motoboy

---
+++ lib/active_shipping/carriers/motoboy.rb
@@ -0,0 +1,22 @@
+# -*- encoding utf-8 -*-
+
+module ActiveShipping
+  class Motoboy < Carrier
+
+    cattr_reader :name
+    @@name = "Motoboy"
+
+    def find_rates(origin, destination, packages, options = {})
+      options = @options.merge(options)
+      if (destination.postal_code.to_i >= 59000001 && destination.postal_code.to_i <= 59161999) || (destination.postal_code.to_i >= 59290000 && destination.postal_code.to_i <= 59290999)
+        success = true
+        rates = [RateEstimate.new(origin, destination, Motoboy.name, "Motoboy", {total_price: 500, currency: "BRL"})]
+      else
+        success = false
+        rates = {}
+        message = "Não entregamos neste endereço"
+      end
+      response = RateResponse.new(success, message, {}, {rates: rates})
+    end
+  end
+end
